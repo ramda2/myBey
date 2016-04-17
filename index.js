9646541f563d14ae8ce13d0ece7fa4e0bdc281a4
skini <skini@mit.edu>
2016-04-16 20:59:56 -0400

    final

--- index.js
+++ index.js
@@ -15,17 +15,18 @@ function onTestChange() {
     // If the user has pressed enter
     if (key == 13) {
         $('#chat').append(makehtmlstring($('#theinput').val(), "me"));
-        $('#theinput').val('');
         $.ajax({
 		  type: "POST",
 		  url: 'http://127.0.0.1:5000/status',
 		  data: {'textmessage': $('#theinput').val() },
+		  
 		  success: function(response){
 			response = JSON.parse(response);		  	
 		  	message = response['message']
 		  	gif = response['gif']
 		  	$('#chat').append(makehtmlstring(message, "them"));
-		  	$('#animation').html('<img src="'+gif+'.gif">');
+		  	$('#animation img').attr('src', gif+'.gif').load(function(){ console.log('done');});
+		  	$('#theinput').val('');
 
 		  }
 		});
